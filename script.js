6b34d863ef926d68d6e41df8940490cf8d5eb443
TkachukNazar <tkachnazar931@gmail.com>
2025-03-15 12:44:22 +0200

    f

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let reg_4 = /^[A-Za-zа-яА-Я]\w*\.(jpg|png|gif)$/;
+let reg = /^[A-Za-zа-яА-Я]\w*\.(jpg|png|gif)$/;
 let info = document.getElementById("info");
 function check() {
   let str = document.getElementById("inp").value.trim();
   console.log(str);
-  if (reg_4.test(str)) {
+  if (reg.test(str)) {
     info.innerHTML = "Ім'я файлу правильне";
   } else {
     info.innerHTML = "Ім'я файлу неправильне";
